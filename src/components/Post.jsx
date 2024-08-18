b97bb78fe7356f9548148e0e0e792a801905b5a1
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-18 10:01:47 +0200

    CSS Styling & CSS Modules

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,12 +1,15 @@
+import postModuleStyles from './Post.module.css';
+
 // const names = ["Glaz", "DODO"];
 
 function Post(props) {
     // const chosenName = Math.random() > 0.5 ? names[0] : names[1];
 
+    // <div style={{color: "red", textAlign: "left"}}>
     return (
-        <div>
-            <p>{props.author}</p>
-            <p>{props.body}</p>
+        <div className={postModuleStyles.post}>
+            <p className={postModuleStyles.author}>{props.author}</p>
+            <p className={postModuleStyles.text}>{props.body}</p>
         </div>
     )
 }
