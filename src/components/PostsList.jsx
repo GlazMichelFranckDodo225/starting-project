fa666272ea04c749e84aa490042e16e2c53a1171
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-09-02 16:01:43 +0200

    Preparing the App For State Management

--- src/components/PostsList.jsx
+++ src/components/PostsList.jsx
@@ -1,12 +1,16 @@
+import NewPost from "./NewPost";
 import Post from "./Post";
 import postsListModuleStyle from './PostsList.module.css';
 
 function PostsList() {
     return (
-        <ul className={postsListModuleStyle.posts}>
-            <Post author="Glaz DODO" body="React is Awesome !" />
-            <Post author="John DOE" body="Hello React World !" />
-        </ul>
+        <>
+            <NewPost />
+            <ul className={postsListModuleStyle.posts}>
+                <Post author="Glaz DODO" body="React is Awesome !" />
+                <Post author="John DOE" body="Hello React World !" />
+            </ul>
+        </>
     )
 }
 
