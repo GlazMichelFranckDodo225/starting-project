dabc9d17f8fce4edc0716fbdfb6575d41911a271
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-09-02 16:31:50 +0200

    Working with State

--- src/components/NewPost.jsx
+++ src/components/NewPost.jsx
@@ -1,13 +1,22 @@
+import { useState } from 'react';
 import classes from './NewPost.module.css';
 
 function NewPost() {
+  // let enteredBody = "";
+  // let stateData = useState('');
+  // stateData[0] // Current Value
+  // stateData[1] // State Updating Function
+  // let [currentValue, setCurrentValue] = useState('');
+  let [enteredBody, setEnteredBody] = useState('');
+  
   // In Regular JS, to Add Event Listener
   /* document.querySelector('change', () => {
 
   }) */
   function changeBodyHandler(event) {
-    console.log(event.target.value); // target ==> "textarea" Element
-    
+    // console.log(event.target.value); // target ==> "textarea" Element
+    // enteredBody = event.target.value;
+    setEnteredBody(event.target.value);
   }
 
   return (
@@ -16,6 +25,7 @@ function NewPost() {
         <label htmlFor="body">Text</label>
         <textarea id="body" required rows={3} onChange={changeBodyHandler} />
       </p>
+      <p>{enteredBody}</p>
       <p>
         <label htmlFor="name">Your name</label>
         <input type="text" id="name" required />
